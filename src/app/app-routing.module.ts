3bc312ab8521b75357f967ea876086a9ddcb7c3e
Martin Albarracin <martin.albarracin@gmail.com>
2020-08-02 21:01:15 -0300

    Children Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PagesRoutingModule } from './pages/pages.routing';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { AuthRoutingModule } from './auth/auth.routing';
 
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 const routes: Routes = [
 
+    // path: '/dashboard' PagesRouting
+   // path: '/auth' AuthRouting
 
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-
+  { path: '', redirectTo:'/dashboard', pathMatch:'full' },
   { path: '**', component: NopagefoundComponent },
 ];
 
@@ -20,7 +19,8 @@ const routes: Routes = [
   declarations: [],
   imports: [
     RouterModule.forRoot(routes),
-    PagesRoutingModule
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [RouterModule],
 })
